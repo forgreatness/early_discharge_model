4dd4263e068460b4f6517cc30d4839e52d0d8c20
Danh Nguyen2 <Danh_Nguyen2@edwards.com>
2024-04-26 16:19:27 -0700

    - added data normalization example to the dataset

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import seaborn as srn
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import normalize
 import tensorflow as tf
 
 encountersDataHeading = np.array(['race', 'gender', 'age', 'time in hospital', 'num procedures', 'num medications', 'readmitted'])
 # 
-encountersData = pd.read_csv('./final_data.csv', names=['race', 'gender', 'age', 'time in hospital', 'num procedures', 'num medications', 'readmitted'], index_col=0, header=None)
+encountersData = pd.read_csv('./final_data.csv', names=['age', 'time in hospital', 'num procedures', 'num medications', 'readmitted', 'race', 'gender'], index_col=0, header=None)
 
 encountersTargetY = encountersData['readmitted']
 encountersTargetX = encountersData.drop(columns = ['readmitted'], axis=1) #remove label readmitted from column axis
@@ -18,12 +19,19 @@ encountersTargetX = encountersData.drop(columns = ['readmitted'], axis=1) #remov
 # Split the data we have into x_train y_train x_test y_test
 x_train, x_test, y_train, y_test = train_test_split(encountersTargetX, encountersTargetY, random_state=42) #random_state should be a seed
 
-scaler = StandardScaler()
+x_train_scaled = x_train
+x_test_scaled = x_test
 
-xScaler = scaler.fit(x_train)
+######### Standardization
+# scaler = StandardScaler()
+# xScaler = scaler.fit(x_train)
+# x_train_scaled = xScaler.transform(x_train)
+# x_test_scaled = xScaler.transform(x_test)
 
-x_train_scaled = xScaler.transform(x_train)
-x_test_scaled = xScaler.transform(x_test)
+######### Normalization
+x_train_scaled = normalize(x_train, axis=0)
+x_test_scaled = normalize(x_test, axis=0)
+
 
 numInputFeatures = len(x_train_scaled[0])
 hiddenLayerOneNumNodes = 7
@@ -36,7 +44,7 @@ nn.add(tf.keras.layers.Dense(units=hiddenLayerTwoNumNodes, activation='relu'))
 nn.add(tf.keras.layers.Dense(units=hiddenLayerThreeNumNodes, activation='relu'))
 nn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
-nn.summary()
+nn.summary()    
 
 nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 nn.fit(x_train_scaled, y_train, validation_split=0.15, epochs=100) # minibatch here
