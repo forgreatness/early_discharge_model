a6748024af356cc23d8f746b6897f64593177cbd
forgreatness <danh1nguyen23@live.com>
2024-04-29 11:08:24 -0700

    - added data augmentation to prep for trying which model will be best - added hyperparameter tuning ideas as well, which will be tried on the decision tree model

--- moreModelTest.py
+++ moreModelTest.py
@@ -81,10 +81,6 @@ print(scaled_x_test)
 # print("show me the count for each unique value", encountersData['readmitted'].value_counts())
 
 
-
-
-
-
 """   MODEL SELECTION   """
 decisionTreeModel = DecisionTreeClassifier(random_state=42)
 randomForestModel = RandomForestClassifier(random_state=42)
@@ -92,17 +88,120 @@ knnModel = KNeighborsClassifier()
 lrModel = LogisticRegression(fit_intercept=True, solver='liblinear', penalty='l1', random_state=42)
 testingModels = [decisionTreeModel, randomForestModel, knnModel, lrModel]
 
-import timeit
+# import timeit
+
+# for model in testingModels:
+#     start = timeit.timeit()
+#     model.fit(scaled_x_train, y_train)
+#     y_pred = model.predict(x_test)
+#     end = timeit.timeit()
+#     print(f'Model: {type(model).__name__}')
+#     print(f'Time to Run: {end - start} seconds')
+#     print(f'Train Score: {model.score(scaled_x_train, y_train)}')
+#     print(f'Test Score: {model.score(scaled_x_test, y_test)}\n')
+
+
+
+"""    MODEL Evaluation     """
+from sklearn.metrics import classification_report
 
 for model in testingModels:
-    start = timeit.timeit()
-    model.fit(scaled_x_train, y_train)
-    y_pred = model.predict(x_test)
-    end = timeit.timeit()
+    model.fit(scaled_x_train, y_train) # fit the model
+    y_pred= model.predict(x_test) # then predict on the test set
     print(f'Model: {type(model).__name__}')
-    print(f'Time to Run: {end - start} seconds')
-    print(f'Train Score: {model.score(scaled_x_train, y_train)}')
-    print(f'Test Score: {model.score(scaled_x_test, y_test)}\n')
+    print(classification_report(y_test, y_pred))
+    
+    
+    
+"""          Cross-Validation            """
+# from sklearn.model_selection import ShuffleSplit
+# from sklearn.model_selection import cross_validate
+
+# rs1 = ShuffleSplit(n_splits=100, test_size=0.03)
+# cv_results_lg = cross_validate(
+#     lrModel, scaled_x_test, y_test, cv=rs1, scoring='accuracy'
+# )
+# lrAccuracyScore = pd.Series(cv_results_lg['test_score'])
+# lrAccuracyScore.plot(
+#     title='Distribution of Logistic Regression accuracy',
+#     kind='box'
+# )
+# plt.show()
+
+# rs2 = ShuffleSplit(n_splits=100, test_size=0.03)
+# cv_results_lg = cross_validate(
+#     decisionTreeModel, scaled_x_test, y_test, cv=rs1, scoring='accuracy'
+# )
+# decisionTreeAccuracyScore = pd.Series(cv_results_lg['test_score'])
+# decisionTreeAccuracyScore.plot(
+#     title='Distribution of Decision Tree accuracy',
+#     kind='box'
+# )
+# plt.show()
+
+# print(
+#     'Average Score: {:.3} [5th percentile: {:.3} & 95th percentile: {:.3}]'.format(
+#     decisionTreeAccuracyScore.mean(),
+#     decisionTreeAccuracyScore.quantile(.05),
+#     decisionTreeAccuracyScore.quantile(.95),
+#     )
+# )
+
+
+"""     Hyperparameter Tuning      """
+# GridSearchCV is a built in module from sklearn that allows us to be able to do hyperparameter tuning
+# print(lrModel.get_params().keys()) # get me all the parameters I can use to build my model 
+# from sklearn.model_selection import GridSearchCV
+
+# lg = LogisticRegression(fit_intercept=True, random_state=24)
+# param_grid = {
+#     'penalty': ['l1', 'l2', 'elasticnet', 'none'],
+#     'solver': ['lbfgs', 'newton-cg', 'liblinear', 'sag', 'saga'],
+#     'max_iter': [100, 1000, 2500, 5000]
+# }
+
+# grid_search = GridSearchCV(lg, param_grid, cv=5, scoring='accuracy', verbose=4)
+# grid_search.fit(scaled_x_train, y_train)
+
+# y_pred = grid_search.predict(scaled_x_test)
+# print(classification_report(y_test, y_pred))
+
+
+"""        Confusion Matrix          """
+# from sklearn.metrics import confusion_matrix
+
+# matrix = confusion_matrix(y_test, y_pred)
+# matrix = matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis]
+
+# plt.figure(figsize=(16,7))
+# srn.set(font_scale=12)
+# srn.heatmap(matrix, annot=True, annot_kws={
+#     'size': 20
+# }, cmap=plt.cm.Greens, linewidths=0.2)
+
+# class_names = ['other', 'readmission']
+# tick_marks = np.arange(len(class_names))
+# tick_marks2 = tick_marks * 0.5
+# plt.xticks(tick_marks, class_names, rotation=0)
+# plt.yticks(tick_marks2, class_names, rotation=0)
+# plt.xlabel('Predicted label')
+# plt.ylabel('True label')
+# plt.title('Confusion Matrix for Logistic Regression Model')
+# plt.show()
+
+
+"""          Data Augmentation              """
+# at this step is either random sampling, SMOTE, updating weights
+from imblearn.over_sampling import SMOTE
+
+smote = SMOTE(random_state=42)
+scaled_x_train_resampled, y_train_resampled = smote.fit_resample(scaled_x_train, y_train)
+# split new resampled dataset to prep for training
+x_train, x_test, y_train, y_test = train_test_split(scaled_x_train_resampled, y_train_resampled, test_size=0.20, random_state=42)
+lr2 = LogisticRegression(fit_intercept=True, penalty='l2')
+lr2.fit(x_train, y_train)
+y_pred = lr2.predict(x_test)
+print(classification_report(y_test, y_pred))
 
 
 """ Data Visualization """
